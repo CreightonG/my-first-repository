33971e099fb8f46792d10cd5bfa79b3abc9726cb
Creighton Gorai <CGorai@Hawaii.edu>
2016-10-15 02:08:37 +0000

    Add a Student.java.

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+  float gps;
+  String firstName;
+  String lastName;
+  String major;
+  int age;
+
+  public Student() {
+
+  }
+
+  public Student(String firstName, String lastname, int age) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  }
+}
